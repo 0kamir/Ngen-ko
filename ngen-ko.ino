17747eb4b487877362d4c4e9b6374b68b43080a9
0kamir <88117226+0kamir@users.noreply.github.com>
2024-12-10 00:39:52 -0300

    ngen-ko.ino

--- ngen-ko.ino
+++ ngen-ko.ino
@@ -1,3 +1,20 @@
+// Ngen-ko
+// Copyright (C) 2024
+//
+// Este programa es software libre: puedes redistribuirlo y/o modificarlo
+// bajo los términos de la Licencia Pública General Affero de GNU
+// publicada por la Free Software Foundation, ya sea la versión 3 de la
+// Licencia, o (a tu elección) cualquier versión posterior.
+//
+// Este programa se distribuye con la esperanza de que sea útil,
+// pero SIN NINGUNA GARANTÍA; ni siquiera la garantía implícita
+// de COMERCIABILIDAD o IDONEIDAD PARA UN PROPÓSITO PARTICULAR.
+// Consulta la Licencia Pública General Affero de GNU para más detalles.
+//
+// Deberías haber recibido una copia de la Licencia Pública General
+// Affero de GNU junto con este programa.
+// Si no, consulta <https://www.gnu.org/licenses/>.
+
 #include <LiquidCrystal.h>
 
 // Pines del LCD Shield
